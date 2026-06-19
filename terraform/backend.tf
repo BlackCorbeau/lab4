588f6abd60dfe11a2572e27f1d0442dc939d366e
Kirill <remizov.kirill2005@yandex.ru>
2026-06-19 14:28:48 +0300

    fixup! feat(tf): Add terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket   = "tf-state-lab4"               # имя твоего бакета (должен существовать)
     key      = "lab4/terraform.tfstate"      # путь к файлу состояния внутри бакета
     region   = "ru-msk"                      # твой регион
-    endpoint = "https://s3.ru-msk.vkcs.cloud"
+    endpoint = "https://hb.ru-msk.vkcloud-storage.ru"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
